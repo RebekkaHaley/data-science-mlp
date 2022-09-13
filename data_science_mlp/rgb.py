80fd0516d98272f45f82339a3ef53df77d19bd21
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-13 17:37:13 +0200

    [docs] reformat to google style & remove imports

--- data_science_mlp/rgb.py
+++ data_science_mlp/rgb.py
@@ -1,30 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+from PIL import Image, ImageDraw
 
 
 class Error(Exception):
-    """Base class for exceptions in this module."""
+    """Base class for exceptions in this module.
+    """
     pass
 
 
 class InputError(Error):
     """Exception raised for errors in the input.
-
-    Attributes:
-        expr -- Input expression in which the error occurred.
-        msg  -- Explanation of the error."""
+    
+    Args:
+        expr (int): Input expression in which the error occurred.
+        msg  (str): Explanation of the error.
+    """
     def __init__(self, expr, msg):
         self.expr = expr
         self.msg = msg
 
 
 class RGB():
-    """Defined with values for RGB as input.
+    """An RGB colour value.
     
-    Attributes:
-        RGB -- Input RGB values should range from 0 to 255.
-        hex -- Automatically converts RGB to hex values."""
+    Args:
+        R (int): Input R value should range from 0 to 255.
+        G (int): Input G value should range from 0 to 255.
+        B (int): Input B value should range from 0 to 255.
+    """
     def __init__(self, R, G, B):
         for X in [R, G, B]:
             if (X < 0) or (X > 255):
@@ -33,17 +37,20 @@ class RGB():
         self.G = G
         self.B = B
         self.RGB = (R, G, B)
+        # Automatically converts RGB to hex values
         self.hex = '#{:02X}{:02X}{:02X}'.format(self.R,self.G,self.B)
 
 
 def generate_RGB_data(X, extreme=False, extreme_magnitude=200):
     """Generates a list filled with X number of RGB class values.
-    Optional: generate cols that are v. dark + v. light for training.
+
+    Can optionally generate cols that are very dark + very light for training.
     
-    Attributes:
-        X -- Number of desired RGB instances.
-        extreme -- Boolean to generate v. dark + v. light cols.
-        extreme_magnitude -- Int between 1 and 254."""
+    Args:
+        X (int): Number of desired RGB instances.
+        extreme (bool): Optional. Generates very dark + very light cols when True.
+        extreme_magnitude (int): Number between 1 and 254.
+    """
     if extreme == True:
         cols = []
         for x in range(X):
@@ -63,14 +70,14 @@ def generate_RGB_data(X, extreme=False, extreme_magnitude=200):
 
 
 def display_RGB_colour(colour, font_col='#000'):
-    """Will draw a box of given colour;
-    and fill with text of given font colour.
+    """Draws a box of a given colour and fills with text of a given font colour.
     
-    Attributes:
-        colour -- String containing a RGB or hex value.
-        font_col -- String containing a RGB or hex value."""
+    Args:
+        colour (str): String containing a RGB or hex value.
+        font_col (str): String containing a RGB or hex value.
+    """
     img = Image.new(mode='RGB', size=(100, 100), color=colour)
     img_draw = ImageDraw.Draw(img)
     img_draw.text((36, 45), 'Text', fill=font_col)
     plt.imshow(img)
-    plt.show();
+    plt.show()
